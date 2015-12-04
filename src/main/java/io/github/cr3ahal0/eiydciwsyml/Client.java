3f711f9daf341d03ae0f1a0b27673ed2beda3828
Maxime BERGEON <webmastercr3a@gmail.com>
2015-12-04 18:27:43 +0100

    Work in Progress : client consumming server

--- src/main/java/io/github/cr3ahal0/eiydciwsyml/Client.java
+++ src/main/java/io/github/cr3ahal0/eiydciwsyml/Client.java
@@ -7,6 +7,7 @@ import javax.naming.NamingException;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by E130110Z on 04/12/15.
@@ -16,15 +17,25 @@ public class Client {
 
     public static void main(String[] args) {
         try {
-            Context initial = new InitialContext();
-            ConnectionFactory cf = (ConnectionFactory) initial.lookup("ConnectionFactory");
+
+            Properties env = new Properties();
+            env.put("java.naming.factory.initial","org.jboss.naming.remote.client.InitialContextFactory");
+            env.put("java.naming.provider.url", "remote://localhost:4447");
+
+            /*env.put(Context.INITIAL_CONTEXT_FACTORY, org.jboss.naming.remote.client.InitialContextFactory.class.getName());
+            env.put(Context.PROVIDER_URL, "remote://localhost:4447");*/
+
+            Context initial = new InitialContext(env);
+            ConnectionFactory cf = (ConnectionFactory) initial.lookup("jms/RemoteConnectionFactory");
             Destination destination = (Destination) initial.lookup("jms/queue.mine");
 
-            Connection connection = cf.createConnection("guest", "guest");
+            //see http://docs.jboss.org/hornetq/2.2.2.Final/user-manual/en/html/configuring-transports.html#configuring-transports.acceptors
+            Connection connection = cf.createConnection();
 
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
             MessageProducer producer = session.createProducer(destination);
+
 
             connection.start();
 
@@ -38,6 +49,7 @@ public class Client {
 
             producer.send(om);
 
+            System.out.println("Sent !");
         } catch (NamingException e) {
             e.printStackTrace();
         } catch (JMSException e) {
